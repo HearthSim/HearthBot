f1ec47ed5c00c2ab8846ff1f4df464737e7a8e4e
Alexander Zeier <alex@hearthsim.net>
2016-12-20 03:22:23 +0100

    implement basic card bot

--- bot.py
+++ bot.py
@@ -3,12 +3,14 @@ import re
 import asyncio
 import json
 from handlers.issue_handler import IssueHandler
+from handlers.card_handler import CardHandler
 
 with open('config.json', 'r') as f:
     config = json.load(f)
 
 client = discord.Client()
 issue_handler = IssueHandler(config["repos"])
+card_handler = CardHandler()
 
 @client.event
 async def on_ready():
@@ -16,6 +18,10 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+	if message.content.startswith("!card "):
+		response = card_handler.handle(message.content[6:])
+		await client.send_message(message.channel, response);
+		return
 	match = re.match("(\w+)?#(\d+)", message.content)
 	if match is not None:
 		prefix = match.group(1)
---
+++ handlers/card_handler.py
@@ -0,0 +1,49 @@
+import re
+from hearthstone.enums import CardType, Race, Rarity
+from hearthstone import cardxml
+
+db, _ = cardxml.load()
+
+
+class CardHandler():	
+	def handle(self, input):
+		print("input:", input)
+		try:
+			card = db[input];
+			if card is not None:
+				return self.stringify_card(card)
+		except Exception as e:
+			print(e)
+			pass
+		
+		try:	
+			index = -1
+			match = re.match("^(.+?)(\d+)$", input)
+			if match is not None:
+				input = match.group(1).strip()
+				index = int(match.group(2))
+			
+			cards = []
+			for card in db.values():
+				if input.lower() in card.name.lower():
+					cards.append(card)
+
+			if index >= 0:
+				return self.stringify_card(cards[index-1], index, len(cards))
+
+			return "\n".join(self.stringify_card(cards[i], i + 1, len(cards)) for i in range(0, 2))
+		except Exception as e:
+			print(e)
+			pass
+		return "Card not found"
+
+	def stringify_card(self, card, index = 0, total = 0):
+		health = card.durability if card.type == CardType.WEAPON else card.health
+		search_index = " (%s/%s)" % (index, total) if total > 0 else ""
+		stats = "%s/%s" % (card.atk, health) if card.atk + health > 0 else ""
+		race = " (%s)" % (card.race.name.title()) if card.race != Race.INVALID else ""
+		rarity = " %s" % card.rarity.name.title() if card.rarity != Rarity.INVALID else ""
+		descr = "\n[%s Mana, %s%s %s%s]" % (card.cost, stats, rarity, card.type.name.title(), race)
+		text = "\n" + card.description if len(card.description) else ""
+		flavor = "\n> " + card.flavortext if len(card.flavortext) else ""
+		return "```Markdown\n[%s][%s]%s%s%s%s\n```" % (card.name, card.id, search_index, descr, text, flavor)
