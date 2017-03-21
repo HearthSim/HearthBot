20764887bc338c2cd366ad11fb41d8932c2326a2
Jerome Leclanche <jerome@leclan.ch>
2017-03-21 02:54:31 +0200

    Add --config argument as a path to the config file

--- bot.py
+++ bot.py
@@ -1,22 +1,32 @@
+#!/usr/bin/env python
+
+import argparse
 import json
 import discord
 from handlers.message_handler import MessageHandler
 
 
-with open("config.json", "r") as f:
-	config = json.load(f)
-client = discord.Client()
-message_handler = MessageHandler(config, client)
+def main():
+	p = argparse.ArgumentParser()
+	p.add_argument("--config", required=True, help="Path to configuration file")
+	args = p.parse_args()
+
+	with open(args.config, "r") as f:
+		config = json.load(f)
+
+	client = discord.Client()
+	message_handler = MessageHandler(config, client)
+
+	@client.event
+	async def on_ready():
+		print("Logged in as", client.user.name)
+
+	@client.event
+	async def on_message(message):
+		await message_handler.handle(message)
+
+	client.run(config["token"])
 
 
-@client.event
-async def on_ready():
-	print("Logged in as", client.user.name)
-
-
-@client.event
-async def on_message(message):
-	await message_handler.handle(message)
-
-
-client.run(config["token"])
+if __name__ == "__main__":
+	main()
