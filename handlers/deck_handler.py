05834dde5b45b96acf42bfb4495f66ac0f7ca980
judge2020 <judge2020@judge2020.com>
2017-08-27 13:32:05 -0400

    Deck linker requested changes

--- handlers/deck_handler.py
+++ handlers/deck_handler.py
@@ -2,47 +2,44 @@ from hearthstone import deckstrings
 import binascii
 import requests
 import json
+import threading
+
+TASK_DELAY = 60 * 60 * 1
 
 class DeckHandler():
 
     def __init__(self, deck_response):
         self.deck_response = deck_response
-        self.decks = self.get_decks()
+        self.decks = []
+        self.update_decks()
+
 
     def handle(self, input):
-        # Handling the message being just the deck string
-        if ' ' not in input:
-            if self.is_valid(input):
-                url = self.get_url(input)
-                if url != '':
-                    return self.deck_response + url
-        # Handling multiple words and multiple deck strings in the message.
         result = ''
         for word in input.split():
-            if self.is_valid(word):
-                result += self.get_url(word)
+            deck = self.get_deck(word)
+            if deck != None:
+                result += self.get_url(deck)
         if result != '':
             return self.deck_response + result
         return None
 
 
     @staticmethod
-    def is_valid(potentialstring):
+    def get_deck(potentialstring):
         try:
-            deckstrings.parse_deckstring(potentialstring)
+            return deckstrings.Deck.from_deckstring(potentialstring)
         except binascii.Error:
-            return False
+            return None
         except ValueError:
-            return False
-        return True
+            return None
 
-    def get_url(self, deckstring):
-        deck = deckstrings.Deck().from_deckstring(deckstring)
+    def get_url(self, deck):
         cards = []
         for cardid, count in deck.get_dbf_id_list():
             for i in range(0, count):
                 cards.append(cardid)
-        request = requests.post("https://hsreplay.net/api/v1/decks/", data={"format": 1, "cards": cards})
+        request = requests.post("https://hsreplay.net/api/v1/decks/", data={"format": int(deck.format), "cards": cards})
         # handles status code 201, when it creates a new deck. No reason to link to a new deck with no stats!
         if request.status_code != 200:
             print("Got status code " + str(request.status_code) + " from HSReplay API.")
@@ -55,20 +52,23 @@ class DeckHandler():
 
         return "https://hsreplay.net/decks/" + shortid + '/\n'
 
-    @staticmethod
-    def get_decks():
+    def update_decks(self):
+        # Start a timer to update decks
+        threading.Timer(TASK_DELAY, self.update_decks).start()
         request = requests.get("https://hsreplay.net/api/v1/analytics/query/list_deck_inventory/")
         if request.status_code != 200:
-            print("Error " + request.status_code + " when preloading HSReplay decks: " + request.text)
-            return []
-        decks = []
+            print("Error " + request.status_code + " when updating HSReplay decks: " + request.text)
+            return
+        olddecks = self.decks
+        self.decks = []
         try:
             for key in json.loads(request.text)["series"]:
-                decks.append(key)
+                self.decks.append(key)
         except:
-            print('Error when preloading decks from HSReplay.')
+            print('Error when updating decks from HSReplay. Reverting update')
+            self.decks = olddecks
             raise
-        print('Successfully preloaded HSReplay decks.')
-        return decks
+
+
 
 
